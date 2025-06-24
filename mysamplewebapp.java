53177714d8d128bbde274cbe6a658983636b5ab1
russest3 <s.p.russell00@gmail.com>
2025-06-24 16:48:23 -0400

    updates

--- mysamplewebapp.java
+++ mysamplewebapp.java
@@ -1,5 +1,13 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+public class Main {
+
+  public static void main(String[] args) {
+    int rows = 5;
+
+    for (int i = 1; i <= rows; ++i) {
+      for (int j = 1; j <= i; ++j) {
+        System.out.print("* ");
+      }
+      System.out.println();
     }
+  }
 }
